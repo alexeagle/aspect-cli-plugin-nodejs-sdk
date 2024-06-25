c2c0dec4c4c2361c34be0abb2632e1ceb86b714b
Alex Eagle <alex@aspect.dev>
2024-06-25 06:59:28 -0700

    fix: implement grpcio service
    go-plugin assumes that the server will return Unavailable or Unimplemented codes, but fastify will instead
    give a standard HTTP error code which isnt' handled:
    https://github.com/hashicorp/go-plugin/blob/721087dad1e61cd41a19a30e5ae740dceb55061d/grpc_stdio.go#L106-L109

--- src/connect.ts
+++ src/connect.ts
@@ -3,6 +3,10 @@ import * as proto from "../proto/aspect_plugin_pb.js";
 import { Plugin } from "../proto/aspect_plugin_connect.js";
 import { Health } from "../proto/grpc/health/v1/health_connect.js";
 import { HealthCheckResponse, HealthCheckResponse_ServingStatus } from "../proto/grpc/health/v1/health_pb.js";
+import {GRPCStdio} from "../proto/plugin/grpc_stdio_connect.js";
+import { Stream } from "stream";
+import { STATUS_CODES } from "http";
+import { StdioData } from "../proto/plugin/grpc_stdio_pb.js";
 
 type FIXME = any;
 
@@ -11,6 +15,11 @@ export default (router: ConnectRouter) => {
     async check(_) {
         return new HealthCheckResponse({status: HealthCheckResponse_ServingStatus.SERVING})
     }
+  });
+  router.service(GRPCStdio as FIXME, {
+    async *streamStdio(_) {
+        //yield 
+      }
   });
   router.service(Plugin as FIXME, {
     async bEPEventCallback(_) {
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import routes from "./connect.js";
 import { Interceptor } from "@connectrpc/connect";
 
 const host = 'localhost';
-const port = 4339;
 
 const logger: Interceptor = (next) => async (req) => {
   console.log(`recevied message on ${req.url}`);
@@ -13,14 +12,29 @@ const logger: Interceptor = (next) => async (req) => {
 };
 
 async function main() {
-  const server = fastify({"http2": true});
+  const server = fastify({http2: true, logger: {
+    level: 'info',
+    // We must not log to stdout since that's the protocol channel
+    // We also can't use stderr until we wire up the GRPCIO service
+    // to send those back to the client to print.
+    file: '/tmp/fastify.log',
+  }});
   await server.register(fastifyConnectPlugin, {
     routes,
   });
-  await server.listen({ host, port });
+  const address = new URL(await server.listen({ host, port: /* auto-select */ 0 }));
+  
   // Protocol handshake, see
   // https://github.com/hashicorp/go-plugin/blob/main/docs/guide-plugin-write-non-go.md#4-output-handshake-information
-  process.stdout.write(`1|3|tcp|127.0.0.1:${port}|grpc\n`);
+  process.stdout.write(`1|3|tcp|127.0.0.1:${address.port}|grpc\n`);
+
+  // FIXME: this isn't working. When running `bazel help` I still get 
+  // 2024-06-25T06:47:10.759-0700 [WARN]  hello-world: plugin failed to exit gracefully
+  // and the client CLI never exits.
+  process.on('SIGQUIT', () => {
+    console.error("Recieved SIGQUIT, terminating http server");
+    server.close();
+  });
 }
 
 void main();
